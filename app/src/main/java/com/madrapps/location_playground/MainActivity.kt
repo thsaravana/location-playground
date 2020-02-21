d05e6ab5686303bf87ee1bc5b4c667ee57114904
thsaravana <th.saravana@gmail.com>
2020-02-21 19:35:16 +0530

    Check for google play services availability and error handling

--- app/src/main/java/com/madrapps/location_playground/MainActivity.kt
+++ app/src/main/java/com/madrapps/location_playground/MainActivity.kt
@@ -2,10 +2,22 @@ package com.madrapps.location_playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val instance = GoogleApiAvailability.getInstance()
+        val googlePlayServicesAvailable = instance.isGooglePlayServicesAvailable(this)
+        Log.d("GoogleApi", "Available = $googlePlayServicesAvailable")
+        val errorDialog = instance.getErrorDialog(this, ConnectionResult.SERVICE_DISABLED, 45)
+        errorDialog.show()
     }
 }
