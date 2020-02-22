527a2b81531a416d1611212721e48a08254e9d54
thsaravana <th.saravana@gmail.com>
2020-02-22 09:09:35 +0530

    Getting the last location via FusedLocationProvider

--- app/src/main/java/com/madrapps/location_playground/MainActivity.kt
+++ app/src/main/java/com/madrapps/location_playground/MainActivity.kt
@@ -1,15 +1,58 @@
 package com.madrapps.location_playground
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import android.util.Log
-import com.google.android.gms.common.ConnectionResult
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.checkSelfPermission
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val LAST_LOCATION_REQUEST = 33
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(ACCESS_COARSE_LOCATION),
+                LAST_LOCATION_REQUEST
+            )
+        } else {
+            getLastLocation()
+        }
+    }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun getLastLocation() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            if (it != null) {
+                txtLocation.text = "Lat = ${it.latitude}, Lon = ${it.longitude}"
+                Log.d("Location", "Lat = ${it.latitude}, Lon = ${it.longitude}")
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LAST_LOCATION_REQUEST) {
+            if ((grantResults.firstOrNull() == PERMISSION_GRANTED)) {
+                getLastLocation()
+            }
+        }
     }
 
     override fun onResume() {
@@ -17,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         val instance = GoogleApiAvailability.getInstance()
         val googlePlayServicesAvailable = instance.isGooglePlayServicesAvailable(this)
         Log.d("GoogleApi", "Available = $googlePlayServicesAvailable")
-        val errorDialog = instance.getErrorDialog(this, ConnectionResult.SERVICE_DISABLED, 45)
-        errorDialog.show()
+        val errorDialog = instance.getErrorDialog(this, googlePlayServicesAvailable, 45)
+        errorDialog?.show()
     }
 }
